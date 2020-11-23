cd553d8e68e03917eab0de6eb992d146d2bb7a3f
nickltheprogrammer <64253617+nickltheprogrammer@users.noreply.github.com>
2020-11-23 10:42:04 +0100

    API

---
+++ RejseplanenAPI/RejseplanenLibrary/Library.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RejseplanenLibrary
+{
+    public class Library
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Hastighed { get; set; }
+        public string Brugernavn { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Library()
+        {
+            
+        }
+
+        public Library(int id, double hastighed, string brugernavn, DateTime timeStamp)
+        {
+            Id = id;
+            Hastighed = hastighed;
+            Brugernavn = brugernavn;
+            TimeStamp = timeStamp;
+        }
+    }
+}
---
+++ RejseplanenAPI/UdpReceiver/Receiver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using RejseplanenConsumer;
+using RejseplanenLibrary;
+
+namespace UdpReceiver
+{
+    class Receiver
+    {
+        private const int Port = 7000;
+
+        public static void Start()
+        {
+            using (UdpClient udpClient = new UdpClient(new IPEndPoint(IPAddress.Any, Port)))
+            {
+                IPEndPoint receiveEndPoint = new IPEndPoint(0, 0);
+
+                try
+                {
+                    Console.WriteLine("Incoming measurements...");
+
+                    while (true)
+                    {
+                        byte[] receiveBytes = udpClient.Receive(ref receiveEndPoint);
+
+                        string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                        Console.WriteLine();
+                        Console.WriteLine($"Sent from {receiveEndPoint.Address} on port number {receiveEndPoint.Port}:");
+
+                        Console.WriteLine(receivedData);
+                        Console.WriteLine(Consumer.PostAsync(JsonConvert.DeserializeObject<Library>(receivedData)).Result ? "POST succeeded" : "POST failed!");
+
+                        Thread.Sleep(500);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+    }
+}
