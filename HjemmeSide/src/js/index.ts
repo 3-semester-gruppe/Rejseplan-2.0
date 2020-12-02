0716cedd62f28edaf03b722ffe100a059b9071af
HappyHeirloom <jonasgross@hotmail.com>
2020-12-02 14:35:59 +0100

    weather added

--- HjemmeSide/src/js/index.ts
+++ HjemmeSide/src/js/index.ts
@@ -1,11 +1,12 @@
 import coord from "proj4";
+import Axios from "../../node_modules/axios/index";
 
 import axios, {
     AxiosResponse,
     AxiosError
 } from "../../node_modules/axios/index"
-axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 
 interface ILibrary {
@@ -48,6 +49,11 @@ Vue.component('library', {
 
 let baseUrl = 'http://localhost:49606/api/Libraries';
 
+let openWeatherBaseUrl = "https://api.openweathermap.org/data/2.5/weather?"
+let openWeatherLat = "lat="
+let openWeatherLong = "&lon="
+let openWeatherApiKey = "&appid=412be2f2e33e80c87ba34e35ac054489"
+
 let rejseplanenbaseurl: string = "http://xmlopen.rejseplanen.dk/bin/rest.exe";
 let format: string = "&format=json";
 
@@ -68,7 +74,7 @@ class Location{
 }
 
 
-new Vue({
+var main = new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
@@ -87,11 +93,16 @@ new Vue({
         afgang_stoppested: [],
         ankomst_stoppested: [],
         selected_afgang: null,
-        selected_ankomst: null
-    },
-    created: function () {
-      // `this` points to the vm instance
-      //this.getLocation()
+        selected_ankomst: null,
+        temperature: null,
+        feels_like: null,
+        wind_speed: null,
+        wind_degree: null,
+        humidity: null,
+        pressure: null,
+        weather_type: "",
+        current_city: "",
+        current_dateTime: ""
     },
     methods: {
         async getLibraryAsync(){
@@ -126,9 +137,6 @@ new Vue({
             let now : Date = new Date(Date.now());
             let deltaTime : number = (departure.getTime() - now.getTime())/(1000 * 3600);
             this.hastighed = Math.round((this.distance / 1000 / deltaTime) * 10)/10;
-        },
-        created() {
-            // this.interval = setInterval(() => this.getHastighed(), 10);
         },
         getAfgangTimeOut() {  
           if (this.timer) {
@@ -190,11 +198,9 @@ new Vue({
         },
         getLocation() {
           navigator.geolocation.getCurrentPosition(position => {  
-            console.log(position.coords.longitude, position.coords.latitude); 
             this.longitude = position.coords.longitude;
             this.latitude = position.coords.latitude;
             //this.afgang = position.coords.longitude.toString() + " "  + position.coords.latitude.toString();
-        
           });
         },
         calculateDistance(lat1: number,lng1: number,lat2: number, lng2: number) {
@@ -217,7 +223,6 @@ new Vue({
             return dist;
         },
         getDistance() {
-
           this.selected_afgang =  this.afgang_stoppested.find( (i: any) => i.name === this.afgang);
           this.selected_ankomst = this.ankomst_stoppested.find ( (i: any) => i.name === this.ankomst); 
 
@@ -228,7 +233,6 @@ new Vue({
           this.distance = Math.round(this.calculateDistance(afgang_Dms[0], afgang_Dms[1], ankomst_Dms[0], ankomst_Dms[1]))
         },
         getDistanceFromLocation() {
-
           navigator.geolocation.getCurrentPosition(position => {  
             console.log(position.coords.longitude, position.coords.latitude); 
             var longitude = position.coords.longitude;
@@ -239,8 +243,42 @@ new Vue({
   
             this.distance = Math.round(this.calculateDistance(latitude, longitude, ankomst_Dms[0], ankomst_Dms[1]));  
           });
+        },
+        async getWeatherFromLatLong(){
+          let weatherData;
+          let dateTime = new Date();
+          let hours = dateTime.getHours().toString();
+          let minutes = dateTime.getMinutes().toString();
+          if(hours.length == 1){
+            hours = "0" + hours;
+          }
+          if(minutes.length == 1){
+            minutes = "0" + minutes;
+          }
+          navigator.geolocation.getCurrentPosition(async position => {  
+            let path = openWeatherBaseUrl + openWeatherLat + position.coords.latitude + openWeatherLong + position.coords.longitude + openWeatherApiKey;
+            await axios
+            .get(path)
+            .then(response => {
+              console.log(response.data);
+              weatherData = response.data.main;
+              this.current_city = response.data.name;
+              this.temperature = (weatherData.temp - 273).toFixed(2);
+              this.feels_like = (weatherData.feels_like - 273).toFixed(2);
+              this.humidity = weatherData.humidity;
+              this.pressure = weatherData.pressure;
+              this.wind_speed = response.data.wind.speed;
+              this.wind_degree = response.data.wind.deg;
+              this.weather_type = response.data.weather[0].main;
+              this.current_dateTime = `${dateTime.toLocaleString('en-us', {  weekday: 'long' })} ${hours + ":" + minutes}`;
+            })
+          });
+
         }
       }
     }
 })
+
+main.getLocation();
+main.getWeatherFromLatLong();
 
