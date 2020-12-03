268d483ada4740d1d6ebf075704515e29580cf44
nickltheprogrammer <64253617+nickltheprogrammer@users.noreply.github.com>
2020-12-03 18:50:14 +0100

    FoelgEnRejse

--- RejseplanenAPI/RejseplanenLibrary/Trip.cs
+++ RejseplanenAPI/RejseplanenLibrary/Trip.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace RejseplanenLibrary
 {
     public class Trip
     {
+        [Key]
         public int Id { get; set; }
         public string UserName { get; set; }
         public string StartDestination { get; set; }
